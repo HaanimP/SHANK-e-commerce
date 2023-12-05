7c14c30bca6e9373c59ce2f6f7a206d5f55e956f
HaanimP <haanimpietersen@gmail.com>
2023-12-05 12:02:23 +0200

    media queries and comments

--- script/product.js
+++ script/product.js
@@ -1,14 +1,18 @@
+// Everything added to cart will add to the string
 const localStorageKeyBought = 'bought';
 
+//getting all the accessories from localstorage which was added on the admin page to store it on the products page
 let bought = JSON.parse(localStorage.getItem(localStorageKeyBought)) || [];
 let main = document.querySelector('.main');
 let accessories = JSON.parse(localStorage.getItem('accessories')) || [];
 
+//if there are no items matching the products declared, a message will appear
 function renderItems(items) {
     if (items.length === 0) {
         main.innerHTML = '<p class="unknown">No items found.</p>';
     } else {
         main.innerHTML = items.map(function (item, index) {
+            //putting the products declared on the admin page into cards on the products page
             return `
             <div class="div">
                 <img class="image" src="${item.url}" alt="${item.name}">
@@ -21,6 +25,7 @@ function renderItems(items) {
     }
 }
 
+//putting everything added to cart into an array
 function add(index) {
     bought.push(accessories[index]);
     localStorage.setItem(localStorageKeyBought, JSON.stringify(bought));
@@ -32,6 +37,7 @@ main.addEventListener('click', function (event) {
     }
 });
 
+// creating the search function to get specific products or sorting it in cheapest to expensive
 function searchAndSort() {
     let searchBar = document.getElementById('searchBar').value.toLowerCase();
     let sortOption = document.getElementById('sortOptions').value;
@@ -52,10 +58,9 @@ function searchAndSort() {
 document.getElementById('searchBar').addEventListener('input', searchAndSort);
 document.getElementById('sortOptions').addEventListener('change', searchAndSort);
 
-// Check if all data is deleted and display a spinner
+// if all products are deleted from the admin page, a spinner will display
 if (accessories.length === 0) {
     main.innerHTML = '<div id="spinner" class="spinner" style:"display: none;"><i class="fas fa-spinner fa-spin"></i></div>';
 } else {
-    // Initial rendering
     renderItems(accessories);
 }
