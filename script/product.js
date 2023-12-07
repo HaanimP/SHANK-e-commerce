1692192269fc31670c9e89db0c9a5baf7865f313
Tohiera Pietersen <haanimpietersen@gmail.com>
2023-12-07 19:02:34 +0200

    try catch productPage

--- script/product.js
+++ script/product.js
@@ -1,53 +1,86 @@
 let localStorageKeyBought = 'bought';
 
-let bought = JSON.parse(localStorage.getItem(localStorageKeyBought)) || [];
+let bought;
+
+try {
+    bought = JSON.parse(localStorage.getItem(localStorageKeyBought)) || [];
+} catch (error) {
+    console.error('Error parsing "bought" from localStorage:', error);
+    bought = [];
+}
+
 let main = document.querySelector('.main');
-let accessories = JSON.parse(localStorage.getItem('accessories')) || [];
+
+let accessories;
+
+try {
+    accessories = JSON.parse(localStorage.getItem('accessories')) || [];
+} catch (error) {
+    console.error('Error parsing "accessories" from localStorage:', error);
+    accessories = [];
+}
 
 function renderItems(items) {
-    if (items.length === 0) {
-        main.innerHTML = '<p class="unknown">No items found.</p>';
-    } else {
-        main.innerHTML = items.map(function (item, index) {
-            return `
-            <div class="div">
-                <img class="image" src="${item.url}" alt="${item.name}">
-                <h2 class="h2">${item.name}</h2>
-                <p class="p">${item.description}</p>
-                <p class="p">R${item.price.toFixed(2)}</p>
-                <button class="cart" value="${index}" data-add>Add to Cart</button>
-            </div>`;
-        }).join('');
+    try {
+        if (items.length === 0) {
+            main.innerHTML = '<p class="unknown">No items found.</p>';
+        } else {
+            main.innerHTML = items.map(function (item, index) {
+                return `
+                <div class="div">
+                    <img class="image" src="${item.url}" alt="${item.name}">
+                    <h2 class="h2">${item.name}</h2>
+                    <p class="p">${item.description}</p>
+                    <p class="p">R${item.price.toFixed(2)}</p>
+                    <button class="cart" value="${index}" data-add>Add to Cart</button>
+                </div>`;
+            }).join('');
+        }
+    } catch (error) {
+        console.error('Error rendering items:', error);
     }
 }
 
 function add(index) {
-    bought.push(accessories[index]);
-    localStorage.setItem(localStorageKeyBought, JSON.stringify(bought));
+    try {
+        bought.push(accessories[index]);
+        localStorage.setItem(localStorageKeyBought, JSON.stringify(bought));
+    } catch (error) {
+        console.error('Error adding item to the cart:', error);
+    }
 }
 
 main.addEventListener('click', function (event) {
-    if (event.target.hasAttribute('data-add')) {
-        add(event.target.value);
+    try {
+        if (event.target.hasAttribute('data-add')) {
+            add(event.target.value);
+        }
+    } catch (error) {
+        console.error('Error handling click event:', error);
     }
 });
 
 function searchAndSort() {
-    let searchBar = document.getElementById('searchBar').value.toLowerCase();
-    let sortOption = document.getElementById('sortOptions').value;
+    try {
+        let searchBar = document.getElementById('searchBar').value.toLowerCase();
+        let sortOption = document.getElementById('sortOptions').value;
 
-    let filteredItems = accessories.filter(item => {
-        return item.name.toLowerCase().includes(searchBar);
-    });
+        let filteredItems = accessories.filter(item => {
+            return item.name.toLowerCase().includes(searchBar);
+        });
 
-    if (sortOption === 'cheapest') {
-        filteredItems.sort((a, b) => a.price - b.price);
-    } else if (sortOption === 'expensive') {
-        filteredItems.sort((a, b) => b.price - a.price);
+        if (sortOption === 'cheapest') {
+            filteredItems.sort((a, b) => a.price - b.price);
+        } else if (sortOption === 'expensive') {
+            filteredItems.sort((a, b) => b.price - a.price);
+        }
+
+        renderItems(filteredItems);
+    } catch (error) {
+        console.error('Error in searchAndSort function:', error);
     }
-
-    renderItems(filteredItems);
 }
+
 
 document.getElementById('searchBar').addEventListener('input', searchAndSort);
 document.getElementById('sortOptions').addEventListener('change', searchAndSort);
