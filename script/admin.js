8354a79707e7542474356b918656c9340b05a9b9
HaanimP <haanimpietersen@gmail.com>
2023-12-08 15:37:20 +0200

    added comments

--- script/admin.js
+++ script/admin.js
@@ -1,7 +1,7 @@
-// Products stored
+// Products stored in the array 
 let accessories = [];
 
-// Constructor function to create items
+// Constructor function to create items to go inside the array
 function Item(id, name, description, price, url) {
     this.id = id;
     this.name = name;
@@ -10,6 +10,7 @@ function Item(id, name, description, price, url) {
     this.url = url;
 }
 
+// creating the products using a constructor function
 let item1 = new Item(
     1,
     'Scrunchies',
@@ -50,18 +51,21 @@ let item5 = new Item(
     'https://i.postimg.cc/mZ696tY5/keychains.jpg'
 );
 
-// Putting accessories in an array
+// Pushing accessories into the array called accessories declared on top
 accessories.push(item1, item2, item3, item4, item5);
 
 function store() {
-    // Setting array to local storage
+    // Setting array to local storage using stringify so that it becomes a string
     localStorage.setItem('accessories', JSON.stringify(accessories));
 }
 
+//the function that will display the products in the array on the table
 function SHANK() {
+  //selecting the table element on the document where the array will be displayed in
     let table = document.querySelector('table');
+    //adding headings as well as the information in the array onto the html document 
     table.innerHTML = `
-        <tr>
+        <tr> 
             <th>ID</th>
             <th>Name</th>
             <th>Price</th>
@@ -70,7 +74,7 @@ function SHANK() {
             <th>Edit</th>
             <th>Delete</th>
         </tr>
-    `;
+    `; // mapping the array to populate the table
     let jewelry = accessories.map(function (item, index) {
         return `<tr>
         <td>${item.id}</td>
@@ -83,8 +87,10 @@ function SHANK() {
         </tr>`;
     });
 
-    table.innerHTML += jewelry.join('');
+    table.innerHTML += jewelry.join(''); // the variable declared to populate the array onto the html
 
+    // declaring the edit button with the class of edit, for each button there is an event listener 
+    // with the attribute of its index, when it is clicked to open the edit form it checks the index in the array
     let editButtons = document.querySelectorAll('.edit');
     editButtons.forEach((button) => {
         button.addEventListener('click', function () {
@@ -93,6 +99,9 @@ function SHANK() {
         });
     });
 
+    // declaring the delete button with the class of delete, for each button there is an event listener 
+    // with the attribute of its index, when it is clicked to delete something it checks the index in the array
+    // and then removes the index and information in the array
     let deleteButtons = document.querySelectorAll('.delete');
     deleteButtons.forEach((button) => {
         button.addEventListener('click', function () {
@@ -101,44 +110,58 @@ function SHANK() {
     });
 }
 
+// removes products in the array when the delete button is clicked and changes the length of the array
+// calling back the function where the products were stored in local storage to adjust the new array
 function remove(position, callback) {
+  // removes one element at the position removed in the array
     accessories.splice(position, 1);
+    //updating the array in local storage
     store();
+    //calling function to do actions after removed
     callback();
 }
 
+// calling the function to display the new array after something was removed out of the array
+// calling the function to remove that item from the table
 store();
 SHANK();
 
 // Function to open the edit form
+//edits the information in the table displaying already by getting the id for the specefic elements
 function openEditForm(item) {
+  // getting back elements from the edit form
     const editNameInput = document.getElementById('editName');
     const editDescriptionInput = document.getElementById('editDescription');
     const editPriceInput = document.getElementById('editPrice');
     const editImageInput = document.getElementById('editImage');
     const editForm = document.getElementById('editForm');
 
+    // if the item has information populate the edit form places to answer
     if (item) {
         editNameInput.value = item.name;
         editDescriptionInput.value = item.description;
         editPriceInput.value = item.price;
         editImageInput.value = item.url;
-        editForm.setAttribute('data-index', item.id); // Store the index in the form
+        // setting the attribute to the items id
+        editForm.setAttribute('data-index', item.id);
     }
-
+  //  style to display the edit form
     editForm.style.display = 'block';
 }
 
-// Event listener for the "Save" button in the edit form
+// whenever the save button is clicked it checks the id of the edit form for the index of the button
 document.getElementById('saveEdit').addEventListener('click', function () {
     const rowIndex = document.getElementById('editForm').getAttribute('data-index');
     
-    // Ensure the index is valid
+    // if the index of the row is not a valid answer there will be an error in the console log and returns
     if (rowIndex === null) {
         console.error("Invalid row index");
         return;
     }
 
+    // the declared variable finds the index in the array by the id
+    // by selecting the element by id 
+    // updating the item with values from the inputs of the edit form
     const selectedItem = accessories.find(item => item.id == rowIndex);
     selectedItem.name = document.getElementById('editName').value;
     selectedItem.description = document.getElementById('editDescription').value;
@@ -152,28 +175,33 @@ document.getElementById('saveEdit').addEventListener('click', function () {
 
 // Event listener for the "Cancel" button in the edit form
 document.getElementById('cancelEdit').addEventListener('click', function () {
+  // when this button is clicked the form closes without saving any changes
     closeEditForm();
 });
 
 // Event listener for the "Save" button in the edit form
 document.getElementById('saveEdit').addEventListener('click', function () {
+  // when this button is clicked it checks the index of the product edited
     const rowIndex = document.getElementById
 
 
-    // Ensure the index is valid
+    // Logs an error if an invalid answer is given and returns
     if (rowIndex === null) {
         console.error("Invalid row index");
         return;
     }
 
+    // checks the id of the product in the array being edited
+    // shows the changes made in that selected index on the form
     const selectedItem = accessories.find(item => item.id == rowIndex);
     selectedItem.name = document.getElementById('editName').value;
     selectedItem.description = document.getElementById('editDescription').value;
     selectedItem.price = parseFloat(document.getElementById('editPrice').value);
     selectedItem.url = document.getElementById('editImage').value;
 
-    // Update the table and close the edit form
+    // Update the table to add the edited information and close the edit form
     SHANK();
+    // closes the edit form after changes made are saved
     closeEditForm();
 });
 
@@ -183,50 +211,58 @@ document.getElementById('cancelEdit').addEventListener('click', function () {
     closeEditForm();
 });
 
-//function to add product
+//when the dom content is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
+  // function to add a product
     function addProduct() {
 
-        // Update storage and display
+        // Update array in local storage
         store();
+        // call the function to see the updated table with the products
         SHANK();
 
-        // Close the add product modal
+        // Close the add product modal once a new product is added
         closeAddProductModal();
     }
 
-    // combine the addProduct function to a button or an event listener
+    // when the button is clicked the add product function is triggered
     document.getElementById('addProductButton').addEventListener('click', addProduct);
 });
 
 // Function to close the edit form modal
 function closeEditForm() {
+  // getting the edit form by its id
     const editForm = document.getElementById('editForm');
     editForm.style.display = 'none';
 }
 
 // Function to close the add product modal
 function closeAddProductModal() {
+  // getting the add product modal by its id
     document.getElementById('addProductModal').style.display = 'none';
 }
 
-
+// when the dom content is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     function addProduct() {
+      // get  input elements for the new product
         const newName = document.getElementById('newName');
         const newImg = document.getElementById('newImg');
         const newDes = document.getElementById('newDes');
         const newPrice = document.getElementById('newPrice');
+        // getting element to display result
         const resultElement = document.getElementById('result');
 
-        // Clear previous results
+        // Clear previous result
         resultElement.innerHTML = "";
 
+        //prevent default submitting
         event.preventDefault();
 
+        // if empty an alert will pop up to put in the information needed 
         if (newName.value === '' || newImg.value === '' || newDes.value === '' || newPrice.value === '') {
             alert('Please fill in all fields to add a new product!');
-        } else {
+        } else { // a new product will be added to the array in the required inputs
             accessories.push(new Item(
                 accessories.length + 1,
                 newName.value,
@@ -235,13 +271,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 newImg.value
             ));
 
-            // Clear input fields
+            // Clear input fields after a new product was added
             newName.value = '';
             newImg.value = '';
             newDes.value = '';
             newPrice.value = '';
 
-            // Update storage and display
+            // Update storage of the array and display on the html
             store();
             SHANK();
         }
@@ -251,12 +287,15 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('addProductButton').addEventListener('click', addProduct);
 });
 
+// when the dom content is fully loaded 
 document.addEventListener('DOMContentLoaded', function () {
     function addProduct() {
+      // getting the input elements for the new product information
         const newName = document.getElementById('newName');
         const newImg = document.getElementById('newImg');
         const newDes = document.getElementById('newDes');
         const newPrice = document.getElementById('newPrice');
+        // element to display result
         const resultElement = document.getElementById('result');
         const spinner = document.getElementById('spinner');
 
@@ -266,17 +305,20 @@ document.addEventListener('DOMContentLoaded', function () {
         // Clear previous results
         resultElement.innerHTML = "";
 
+        // prevent default submitting
         event.preventDefault();
 
+        // return an element if an imput is not found
         if (!newName || !newImg || !newDes || !newPrice) {
             console.error('One or more input elements not found.');
             return;
         }
 
+        // if a new product is added an alert will show
         if (newName.value === '' || newImg.value === '' || newDes.value === '' || newPrice.value === '') {
             alert('You have added a new product!');
         } else {
-            // Simulating an asynchronous operation (e.g., an API call)
+            // Simulating an asynchronous operation (
             setTimeout(function () {
                 accessories.push(new Item(
                     accessories.length + 1,
@@ -300,7 +342,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 setTimeout(function () {
                     spinner.style.display = 'none';
                 }, 5000);
-            }, 1000); // Simulating a delay of 1 second (adjust as needed)
+            }, 1000); // Simulating a delay of 1 second 
         }
     }
 
