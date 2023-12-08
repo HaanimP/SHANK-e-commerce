4806520b067914d6499ee892d21dde9bd5e9e5f5
HaanimP <haanimpietersen@gmail.com>
2023-12-08 09:11:42 +0200

    fixed indentation

--- script/product.js
+++ script/product.js
@@ -1,94 +1,94 @@
-let localStorageKeyBought = 'bought';
+    let localStorageKeyBought = 'bought';
 
-let bought;
+    let bought;
 
-try {
-    bought = JSON.parse(localStorage.getItem(localStorageKeyBought)) || [];
-} catch (error) {
-    console.error('Error parsing "bought" from localStorage:', error);
-    bought = [];
-}
-
-let main = document.querySelector('.main');
-
-let accessories;
-
-try {
-    accessories = JSON.parse(localStorage.getItem('accessories')) || [];
-} catch (error) {
-    console.error('Error parsing "accessories" from localStorage:', error);
-    accessories = [];
-}
-
-function renderItems(items) {
     try {
-        if (items.length === 0) {
-            main.innerHTML = '<p class="unknown">No items found.</p>';
-        } else {
-            main.innerHTML = items.map(function (item, index) {
-                return `
-                <div class="div">
-                    <img class="image" src="${item.url}" alt="${item.name}">
-                    <h2 class="h2">${item.name}</h2>
-                    <p class="p">${item.description}</p>
-                    <p class="p">R${item.price.toFixed(2)}</p>
-                    <button class="cart" value="${index}" data-add>Add to Cart</button>
-                </div>`;
-            }).join('');
+        bought = JSON.parse(localStorage.getItem(localStorageKeyBought)) || [];
+    } catch (error) {
+        console.error('Error parsing "bought" from localStorage:', error);
+        bought = [];
+    }
+
+    let main = document.querySelector('.main');
+
+    let accessories;
+
+    try {
+        accessories = JSON.parse(localStorage.getItem('accessories')) || [];
+    } catch (error) {
+        console.error('Error parsing "accessories" from localStorage:', error);
+        accessories = [];
+    }
+
+    function renderItems(items) {
+        try {
+            if (items.length === 0) {
+                main.innerHTML = '<p class="unknown">No items found.</p>';
+            } else {
+                main.innerHTML = items.map(function (item, index) {
+                    return `
+                    <div class="div">
+                        <img class="image" src="${item.url}" alt="${item.name}">
+                        <h2 class="h2">${item.name}</h2>
+                        <p class="p">${item.description}</p>
+                        <p class="p">R${item.price.toFixed(2)}</p>
+                        <button class="cart" value="${index}" data-add>Add to Cart</button>
+                    </div>`;
+                }).join('');
+            }
+        } catch (error) {
+            console.error('Error rendering items:', error);
         }
-    } catch (error) {
-        console.error('Error rendering items:', error);
     }
-}
 
-function add(index) {
-    try {
-        bought.push(accessories[index]);
-        localStorage.setItem(localStorageKeyBought, JSON.stringify(bought));
-    } catch (error) {
-        console.error('Error adding item to the cart:', error);
-    }
-}
-
-main.addEventListener('click', function (event) {
-    try {
-        if (event.target.hasAttribute('data-add')) {
-            add(event.target.value);
+    function add(index) {
+        try {
+            bought.push(accessories[index]);
+            localStorage.setItem(localStorageKeyBought, JSON.stringify(bought));
+        } catch (error) {
+            console.error('Error adding item to the cart:', error);
         }
-    } catch (error) {
-        console.error('Error handling click event:', error);
     }
-});
 
-function searchAndSort() {
-    try {
-        let searchBar = document.getElementById('searchBar').value.toLowerCase();
-        let sortOption = document.getElementById('sortOptions').value;
-
-        let filteredItems = accessories.filter(item => {
-            return item.name.toLowerCase().includes(searchBar);
-        });
-
-        if (sortOption === 'cheapest') {
-            filteredItems.sort((a, b) => a.price - b.price);
-        } else if (sortOption === 'expensive') {
-            filteredItems.sort((a, b) => b.price - a.price);
+    main.addEventListener('click', function (event) {
+        try {
+            if (event.target.hasAttribute('data-add')) {
+                add(event.target.value);
+            }
+        } catch (error) {
+            console.error('Error handling click event:', error);
         }
+    });
 
-        renderItems(filteredItems);
-    } catch (error) {
-        console.error('Error in searchAndSort function:', error);
+    function searchAndSort() {
+        try {
+            let searchBar = document.getElementById('searchBar').value.toLowerCase();
+            let sortOption = document.getElementById('sortOptions').value;
+
+            let filteredItems = accessories.filter(item => {
+                return item.name.toLowerCase().includes(searchBar);
+            });
+
+            if (sortOption === 'cheapest') {
+                filteredItems.sort((a, b) => a.price - b.price);
+            } else if (sortOption === 'expensive') {
+                filteredItems.sort((a, b) => b.price - a.price);
+            }
+
+            renderItems(filteredItems);
+        } catch (error) {
+            console.error('Error in searchAndSort function:', error);
+        }
     }
-}
 
 
-document.getElementById('searchBar').addEventListener('input', searchAndSort);
-document.getElementById('sortOptions').addEventListener('change', searchAndSort);
+    document.getElementById('searchBar').addEventListener('input', searchAndSort);
+    document.getElementById('sortOptions').addEventListener('change', searchAndSort);
 
-// Check if all data is deleted and display a spinner
-if (accessories.length === 0) {
-    main.innerHTML = '<div id="spinner" class="spinner" style:"display: none;"><i class="fas fa-spinner fa-spin"></i></div>';
-} else {
-    // Initial rendering
-    renderItems(accessories);
-}
+    // Check if all data is deleted and display a spinner
+    if (accessories.length === 0) {
+        main.innerHTML = '<div id="spinner" class="spinner" style:"display: none;"><i class="fas fa-spinner fa-spin"></i></div>';
+    } else {
+        // Initial rendering
+        renderItems(accessories);
+    }
